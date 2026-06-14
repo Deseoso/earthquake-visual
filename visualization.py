f44d2d1128c51e109786324dd1d34523d9c08ce9
Mihei <miheimiheev2009@gmail.com>
2026-06-14 15:46:50 +0300

    Visualized earthquakes on a map

--- visualization.py
+++ visualization.py
@@ -7,3 +7,16 @@ eq_data = get_earthquakes()
 lons = eq_data['lons']
 lats = eq_data['lats']
 mags = eq_data['mags']
+
+data = {
+    'type': 'scattergeo',
+    'lon': lons,
+    'lat': lats,
+}
+
+my_layout = {
+    'title': 'Earthquake Visualization'
+}
+
+fig = {'data': data, 'layout': my_layout}
+offline.plot(fig, filename='earthquake_visualization.html')
