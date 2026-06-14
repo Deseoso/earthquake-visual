81fd0807b6735c9576073002c241a7b80e0212bf
Mihei <miheimiheev2009@gmail.com>
2026-06-14 14:54:59 +0300

    Initialize earthquake data lists

--- main.py
+++ main.py
@@ -13,3 +13,15 @@ data = requests.get(url, params=params).json()
 
 print(len(data['features']))
 print(data['features'][0])
+
+for f in data['features'][:5]:
+    print(f['properties']['mag'], f['properties']['place'])
+
+lons, lats, mags  = [], [], []
+for f in data['features']:
+    coords = f['geometry']['coordinates']
+
+    lons.append(coords[0])
+    lats.append(coords[1])
+    mags.append(f['properties']['mag'])
+
